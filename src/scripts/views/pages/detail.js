8b14d57f0bf7fc16f0c6912b723853731b9c0ffc
ryhnpngst <rayhanwibowo24@gmail.com>
2023-05-17 19:29:40 +0700

    Initial commit

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,20 @@
+import UrlParser from "../../routes/url-parser";
+import TheMovieDbSource from "../../data/themoviedb-source";
+import { createMovieDetailTemplate } from "../templates/template-creator";
+
+const Detail = {
+    async render() {
+        return `
+            <div id="movie" class="movie"></div>
+        `;
+    },
+
+    async afterRender() {
+        const url = UrlParser.parseActiveUrlWithoutCombiner();
+        const movies = await TheMovieDbSource.detailMovie();
+        const moviesContainer = document.querySelector("#movie");
+        moviesContainer.innerHTML = createMovieDetailTemplate(movies);
+    },
+};
+
+export default Detail;
