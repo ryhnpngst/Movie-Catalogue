9a9c62cbcc68c6d05df9ddedd9694e16c4634c27
ryhnpngst <rayhanwibowo24@gmail.com>
2023-05-26 19:53:25 +0700

    Add Like Movies use IndexedDB

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import UrlParser from '../../routes/url-parser';
 import TheMovieDbSource from '../../data/themoviedb-source';
 import { createMovieDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
     return `
             <div id="movie" class="movie"></div>
+            <div id="likeButtonContainer"></div>
         `;
   },
 
@@ -14,6 +16,17 @@ const Detail = {
     const movies = await TheMovieDbSource.detailMovie(url.id);
     const moviesContainer = document.querySelector('#movie');
     moviesContainer.innerHTML = createMovieDetailTemplate(movies);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movies.id,
+        title: movies.title,
+        overview: movies.overview,
+        backdrop_path: movies.backdrop_path,
+        vote_average: movies.vote_average,
+      },
+    });
   },
 };
 
